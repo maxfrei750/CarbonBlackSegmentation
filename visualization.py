7a58cb7dd054912474248050f35341a36e8fec1d
Max Frei <max.frei@uni-due.de>
2020-07-08 16:53:53 +0200

    Added way to visualize ground truth and prediciton at once.

--- visualization.py
+++ visualization.py
@@ -2,11 +2,26 @@ import numpy as np
 from PIL import Image, ImageFilter
 
 
-def get_overlay_image(input_image, mask, outline_color=(255, 0, 0)):
-    input_image = Image.fromarray(input_image).convert("L").convert("RGB")
+def get_overlay_image(image, ground_truth=None, prediction=None):
+    assert not (
+        ground_truth is None or prediction is None
+    ), "Either ground_truth or prediction need to be specified."
+
+    image = np.array(Image.fromarray(image).convert("RGB"))
+
+    if ground_truth is not None:
+        image = _add_overlay(image, ground_truth, (0, 255, 0))
+
+    if prediction is not None:
+        image = _add_overlay(image, prediction, (255, 0, 0))
+
+    return image
+
+
+def _add_overlay(image, mask, color):
     mask = Image.fromarray(mask).convert("L")
-    mask_outlines = mask.filter(ImageFilter.FIND_EDGES)
-    mask_outlines = np.array(mask_outlines)
-    overlay_image = np.array(input_image)
-    overlay_image[np.nonzero(mask_outlines)] = outline_color
-    return overlay_image
+    outlines = mask.filter(ImageFilter.FIND_EDGES)
+    outlines = np.array(outlines)
+    image[np.nonzero(outlines)] = color
+
+    return image
