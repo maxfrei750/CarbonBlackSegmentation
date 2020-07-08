cbac64e687651d78b562a1300336c545e2f902df
Max Frei <max.frei@uni-due.de>
2020-07-09 01:30:15 +0200

    Adapted evaluation script.

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,28 +1,47 @@
+import os
+
 import numpy as np
 from PIL import Image
 
 import segmentation_models_pytorch as smp
 import torch
 from data import SegmentationDataset
+from ignite.handlers import Checkpoint
+from modeling import get_model
 from transforms import get_preprocessing, get_validation_augmentation
 from visualization import get_overlay_image
 
 
-def get_example_predictions():
-    best_model = torch.load("output/2020-07-08_16-12-59/best_model.pth")
+def checkpoint_path_to_config(checkpoint_path):
+    keys = ["architecture", "encoder", "encoder_weights"]
+    values = os.path.basename(os.path.dirname(checkpoint_path)).split("_")[0].split("-")
+    return dict(zip(keys, values))
 
-    preprocessing_fn = smp.encoders.get_preprocessing_fn("resnet50", "imagenet")
+
+def get_example_predictions():
+    checkpoint_path = "output/FPN-resnet50-imagenet_backend-None-1_20200709-005112/best_model_9_validation_accuracy=0.0301.pt"
+    data_root = "/home/frei/sciebo/Dissertation/Referenzdaten/UBC_TEM"
+    subset = "val"
+
+    config = checkpoint_path_to_config(checkpoint_path)
+
+    model = get_model(config)
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    Checkpoint.load_objects(to_load={"model": model}, checkpoint=checkpoint)
+    model.eval()
+
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(
+        config["encoder"], config["encoder_weights"]
+    )
 
     dataset = SegmentationDataset(
-        "/home/frei/sciebo/Dissertation/Referenzdaten/UBC_TEM",
-        "test",
+        data_root,
+        subset,
         preprocessing=get_preprocessing(preprocessing_fn),
         augmentation=get_validation_augmentation(),
     )
 
-    dataset_vis = SegmentationDataset(
-        "/home/frei/sciebo/Dissertation/Referenzdaten/UBC_TEM", "test"
-    )
+    dataset_vis = SegmentationDataset(data_root, subset)
 
     for i in range(3):
         n = np.random.choice(len(dataset))
@@ -33,7 +52,9 @@ def get_example_predictions():
         gt_mask = gt_mask.squeeze()
 
         x_tensor = torch.from_numpy(image).to("cuda").unsqueeze(0)
-        pr_mask = best_model.predict(x_tensor)
+
+        with torch.no_grad():
+            pr_mask = model(x_tensor)
         pr_mask = pr_mask.squeeze().cpu().numpy().round()
 
         overlay_image = get_overlay_image(image_vis, prediction=pr_mask, ground_truth=gt_mask)
