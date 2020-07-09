67b60339be328d3de99baf49a7c880cde59dab0a
Max Frei <max.frei@uni-due.de>
2020-07-09 16:13:12 +0200

    Added metric: confusion matrix

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,5 @@
 import torch
-from ignite.metrics import Accuracy, ConfusionMatrix, IoU, Loss
+from ignite.metrics import Accuracy, ConfusionMatrix, Loss
 
 
 # TODO: Fix metrics.
@@ -12,15 +12,20 @@ def get_metrics(loss):
 
         return y_pred, y
 
-    def _output_transform_iou(output):
+    def _output_transform_confusion_matrix(output):
         y_pred, y = output
-        y_pred = torch.round(y_pred).byte().squeeze()
+
+        y_pred = y_pred.flatten(1, -1)
         y_pred = torch.stack([y_pred, 1 - y_pred], 1)
+        y = y.flatten(1, -1)
+
         return y_pred, y
 
     metrics = {
         "loss": Loss(loss),
         "accuracy": Accuracy(_output_transform_accuracy),
-        # "iou": IoU(ConfusionMatrix(num_classes=2, output_transform=_output_transform_iou)),
+        "confusion matrix": ConfusionMatrix(
+            num_classes=2, output_transform=_output_transform_confusion_matrix, average="precision"
+        ),
     }
     return metrics
