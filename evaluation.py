faac786fdb3c448e87958a39e69cf3925970d803
Max Frei <max.frei@uni-due.de>
2020-07-10 15:23:32 +0200

    Added class ExamplePredictionLogger.

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,8 @@
+import random
+
 import torch
 from ignite.metrics import Accuracy, ConfusionMatrix, Loss
+from visualization import get_overlay_image
 
 
 def get_metrics(loss):
@@ -28,3 +31,30 @@ def get_metrics(loss):
         ),
     }
     return metrics
+
+
+class ExamplePredictionLogger:
+    def __init__(self, tb_logger, model, device):
+        self.model = model
+        self.tb_logger = tb_logger
+        self.device = device
+        self.dataset = None
+        self.image_index = None
+
+    def log_visualization(self, dataset, epoch):
+        if dataset != self.dataset:
+            self.dataset = dataset
+            self.image_index = random.randint(0, len(dataset) - 1)
+
+        image, mask_gt, image_vis = dataset.get_example_sample(self.image_index)
+
+        image = torch.from_numpy(image).to(self.device).unsqueeze(0)
+
+        with torch.no_grad():
+            mask_pred = self.model(image).cpu().numpy().squeeze().round()
+
+        overlay_image = get_overlay_image(image_vis, mask_gt, mask_pred)
+
+        self.tb_logger.writer.add_image(
+            "example prediction", overlay_image, epoch, dataformats="HWC"
+        )
