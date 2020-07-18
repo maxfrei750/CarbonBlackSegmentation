c7def8b30fee12dd74a34b3cb6afc08e093e61ec
Max Frei <max.frei@uni-due.de>
2020-07-18 11:11:43 +0200

    Increased crop size: 320 => 640

--- transforms.py
+++ transforms.py
@@ -4,12 +4,15 @@ import albumentations as albu
 
 
 def get_training_augmentation():
+    crop_size = 640
     train_transform = [
         albu.HorizontalFlip(p=0.5),
         albu.VerticalFlip(p=0.5),
         albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
-        albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
-        albu.RandomCrop(height=320, width=320, always_apply=True),
+        albu.PadIfNeeded(
+            min_height=crop_size, min_width=crop_size, always_apply=True, border_mode=0
+        ),
+        albu.RandomCrop(height=crop_size, width=crop_size, always_apply=True),
         albu.IAAAdditiveGaussianNoise(p=0.2),
         albu.IAAPerspective(p=0.5),
         albu.OneOf([albu.CLAHE(p=1), albu.RandomBrightness(p=1), albu.RandomGamma(p=1)], p=0.9),
