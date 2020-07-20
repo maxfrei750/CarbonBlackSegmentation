9d24f0d0389c06127e2654391e514bd9ba36ad65
Max Frei <max.frei@uni-due.de>
2020-07-20 10:36:39 +0200

    Refactoring.

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,12 +3,9 @@ import os
 import numpy as np
 from PIL import Image
 
-import segmentation_models_pytorch as smp
-import torch
+import fire
 from data import SegmentationDataset
-from ignite.handlers import Checkpoint
-from modeling import get_model
-from transforms import get_preprocessing, get_validation_augmentation
+from deployment import Segmentor
 from visualization import get_overlay_image
 
 
@@ -18,48 +15,31 @@ def checkpoint_path_to_config(checkpoint_path):
     return dict(zip(keys, values))
 
 
-def get_example_predictions():
-    checkpoint_path = "output/FPN-resnet50-imagenet_backend-None-1_20200709-005112/best_model_9_validation_accuracy=0.0301.pt"
-    data_root = "/home/frei/sciebo/Dissertation/Referenzdaten/UBC_TEM"
-    subset = "val"
-
+def get_example_predictions(
+    data_root, checkpoint_path, data_subset="val", device="cuda", num_predictions=3
+):
     config = checkpoint_path_to_config(checkpoint_path)
 
-    model = get_model(config)
-    checkpoint = torch.load(checkpoint_path, map_location="cpu")
-    Checkpoint.load_objects(to_load={"model": model}, checkpoint=checkpoint)
-    model.eval()
-
-    preprocessing_fn = smp.encoders.get_preprocessing_fn(
-        config["encoder"], config["encoder_weights"]
+    segmentor = Segmentor(
+        architecture=config["architecture"],
+        encoder=config["encoder"],
+        encoder_weights=config["encoder_weights"],
+        device=device,
     )
 
-    dataset = SegmentationDataset(
-        data_root,
-        subset,
-        preprocessing=get_preprocessing(preprocessing_fn),
-        augmentation=get_validation_augmentation(),
-    )
+    dataset = SegmentationDataset(data_root, data_subset)
+    dataset_vis = SegmentationDataset(data_root, data_subset)
 
-    dataset_vis = SegmentationDataset(data_root, subset)
+    for i in range(num_predictions):
+        image_id = np.random.choice(len(dataset))
 
-    for i in range(3):
-        n = np.random.choice(len(dataset))
+        image_vis = dataset_vis[image_id][0].astype("uint8")
+        image, mask_gt = dataset[image_id]
 
-        image_vis = dataset_vis[n][0].astype("uint8")
-        image, gt_mask = dataset[n]
-
-        gt_mask = gt_mask.squeeze()
-
-        x_tensor = torch.from_numpy(image).to("cuda").unsqueeze(0)
-
-        with torch.no_grad():
-            pr_mask = model(x_tensor)
-        pr_mask = pr_mask.squeeze().cpu().numpy().round()
-
-        overlay_image = get_overlay_image(image_vis, prediction=pr_mask, ground_truth=gt_mask)
+        mask_pred = segmentor.segment_image(image)
+        overlay_image = get_overlay_image(image_vis, prediction=mask_pred, ground_truth=mask_gt)
         Image.fromarray(overlay_image).show()
 
 
 if __name__ == "__main__":
-    get_example_predictions()
+    fire.Fire(get_example_predictions)
