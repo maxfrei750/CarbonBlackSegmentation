ecda339fe0064875defee0cdd7cfe55ce6b5c3ca
Timothy A. Sipkens <tsipkens@gmail.com>
2020-10-26 16:29:51 -0400

    Added viz_binaries to visualization module
    Originally written for a Jupyter notebook, this function creates a grid of classified images. Results may be slow if a large number of images are to be plotted.

--- visualization.py
+++ visualization.py
@@ -55,3 +55,44 @@ def plot_confusion_matrix(confusion_matrix_data):
     plt.tight_layout()
 
     return plt.gcf()
+
+
+#== VIZ_BINARIES ============================================#
+# Function to plot a grid of restults.
+# Takes masks and option image_paths argument. If:
+#     Only masks, plots B&W binaries. 
+#     Both masks and image_paths, uses get_overlay_image above.
+# Author: Timothy Sipkens
+def viz_binaries(masks, image_paths=''):
+    plt.gray() # apply grayscale colormap
+    
+    no_masks = len(masks) # number of images/masks
+    no_col = np.minimum(no_masks, 5) # max. no. of columns is 5
+    no_row = int(np.ceil(no_masks / no_col)) # split up over rows
+    fig, axs = plt.subplots(no_row, no_col, figsize=(20, 3.5*no_row))
+    
+    ii = 0 # column
+    jj = 0 # row
+    kk = 0 # global
+    for mask in masks:
+        axs[jj,ii].set_xticks([])
+        axs[jj,ii].set_yticks([])
+        
+        if image_paths=='': 
+            axs[jj,ii].imshow(mask)
+        else: 
+            image = np.asarray(Image.open(image_paths[kk]).convert("RGB"))
+            axs[jj,ii].imshow(get_overlay_image(image, prediction=mask))
+            
+        ii = ii + 1
+        kk = kk + 1
+        if ii==no_col:
+            ii = 0
+            jj = jj + 1
+    
+    # for remaining panels, remove axes
+    while (ii*jj) <= ((no_col-1)*(no_row-1)):
+        axs[jj,ii].axis('off')
+        ii = ii + 1
+        
+    return None
